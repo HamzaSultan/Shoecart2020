3573f575f520adcbafdb2014e0ee3c1fdb5f6f58
HamzaSultan <56472325+HamzaSultan@users.noreply.github.com>
2020-08-02 14:12:29 +0500

    initial commit

---
+++ src/components/section/Payment.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Payment extends Component {
+    render() {
+        return (
+            <div>
+                <h2 style={{textAlign: "center" ,color:" #03c2fc"}}>Payment Component</h2>
+            </div>
+        )
+    }
+}
+
+export default Payment
